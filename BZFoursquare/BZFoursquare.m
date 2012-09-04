2ba238c6798c3dd39ebc5223f08095ded86bf451
Goichi Hirakawa <gooichi@gyazsquare.com>
2012-09-05 00:10:47 +0900

    Added the "init" initializer

--- BZFoursquare/BZFoursquare.m
+++ BZFoursquare/BZFoursquare.m
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2011 Ba-Z Communication Inc. All rights reserved.
+ * Copyright (C) 2011-2012 Ba-Z Communication Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -49,6 +49,10 @@
 @synthesize locale = locale_;
 @synthesize sessionDelegate = sessionDelegate_;
 @synthesize accessToken = accessToken_;
+
+- (id)init {
+    return [self initWithClientID:nil callbackURL:nil];
+}
 
 - (id)initWithClientID:(NSString *)clientID callbackURL:(NSString *)callbackURL {
     NSParameterAssert(clientID != nil && callbackURL != nil);
