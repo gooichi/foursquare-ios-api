1f598f6821e3aecc47c3227207e583bb2167a13c
Goichi Hirakawa <gooichi@gyazsquare.com>
2013-08-14 21:13:21 +0900

    Add support for an operation queue to use for delegate messages

---
+++ FSQDemo/BZFoursquareLogicTests/BZFoursquareLogicTests.m
@@ -0,0 +1,63 @@
+//
+//  BZFoursquareLogicTests.m
+//  BZFoursquareLogicTests
+//
+
+#import "BZFoursquareLogicTests.h"
+
+#ifndef NSFoundationVersionNumber_iOS_5_1
+#define NSFoundationVersionNumber_iOS_5_1  890.1
+#endif
+
+@interface BZFoursquareLogicTests ()
+@property(nonatomic,retain) NSConditionLock *lock;
+@end
+
+@implementation BZFoursquareLogicTests
+
+- (void)setUp {
+    [super setUp];
+    // Set-up code here.
+}
+
+- (void)tearDown {
+    // Tear-down code here.
+    [super tearDown];
+}
+
+- (void)testThatMakesSureWeDontFinishTooFast {
+    // Avoid "Tests did not finish" warning
+    [NSThread sleepForTimeInterval:1];
+}
+
+- (void)testDelegateQueue {
+    BZFoursquareRequest *request = [[BZFoursquareRequest alloc] initWithPath:@"venues/search" HTTPMethod:@"GET" parameters:@{@"ll": @"40.7,-74"} delegate:self];
+    request.delegateQueue = [[NSOperationQueue alloc] init];
+    if (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_5_1) {
+        STAssertThrows([request start], @"");
+    } else {
+        self.lock = [[NSConditionLock alloc] initWithCondition:0];
+        STAssertNoThrow([request start], @"");
+        [self.lock lockWhenCondition:1];
+    }
+}
+
+#pragma mark - BZFoursquareRequestDelegate
+
+- (void)requestDidStartLoading:(BZFoursquareRequest *)request {
+    NSLog(@"%s", __PRETTY_FUNCTION__);
+}
+
+- (void)requestDidFinishLoading:(BZFoursquareRequest *)request {
+    NSLog(@"%s", __PRETTY_FUNCTION__);
+    [self.lock lock];
+    [self.lock unlockWithCondition:1];
+}
+
+- (void)request:(BZFoursquareRequest *)request didFailWithError:(NSError *)error {
+    NSLog(@"%s: %@", __PRETTY_FUNCTION__, error);
+    [self.lock lock];
+    [self.lock unlockWithCondition:1];
+}
+
+@end
